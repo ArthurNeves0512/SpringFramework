3a6d3f290d27d6bf62b4204d105e87351e666379
arthur <arthurcontroleambiental@gmail.com>
2022-10-03 21:55:48 -0300

    aplicacao spring.io

---
+++ spring/src/main/java/guru/springframework/spring5web/ServletInitializer.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring5web;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Spring5webApplication.class);
+	}
+
+}
